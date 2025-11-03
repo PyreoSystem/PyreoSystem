47a26cf21e819434572dbcbe4c0fbf4d22dd7ac2
PyreoSystem <pyreosystem@gmail.com>
2025-11-03 16:07:51 -0500

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", padding: 24 }}>
+      <h1>Global Hub</h1>
+      <p>It works! Your Next.js app is live on Vercel.</p>
+    </div>
+  );
+}
